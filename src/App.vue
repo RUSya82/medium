fbbdf8301149ef5803d56f00a39ef4111a2abe68
RuslanSalimov <mail-spam2012@yandex.ru>
2023-10-15 17:13:19 +0500

    Create authorization token in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    Hello medium!
+    <top-bar-component/>
       <router-view/>
   </div>
 </template>
+<script>
+import TopBarComponent from '@/components/TopBarComponent';
+import { actionTypes } from '@/store/modules/auth';
+
+export default {
+    components: {
+        TopBarComponent,
+    },
+    mounted() {
+        this.$store.dispatch(actionTypes.getCurrentUser)
+    }
+}
+
+</script>
 
 
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -7,15 +7,37 @@ export const  mutationTypes = {
     registerFailed: '[auth] registerFailed',
     loginStart: '[auth] loginStart',
     loginSuccess: '[auth] loginSuccess',
-    loginFailed: '[auth] loginFailed'
+    loginFailed: '[auth] loginFailed',
+    getCurrentUserStart: '[auth] getCurrentUserStart',
+    getCurrentUserSuccess: '[auth] getCurrentUserSuccess',
+    getCurrentUserFailed: '[auth] getCurrentUserFailed'
 }
 export const actionTypes = {
     register: '[auth] register',
-    login: '[auth] login'
+    login: '[auth] login',
+    getCurrentUser: '[auth] getCurrentUser'
+}
+
+export const getterTypes = {
+    currentUser: '[auth] currentUser',
+    isLoggedIn: '[auth] isLoggedIn',
+    isAnonymous: '[auth] isAnonymous'
 }
 const auth = {
+    getters:{
+        [getterTypes.currentUser](state) {
+            return state.currentUser;
+        },
+        [getterTypes.isLoggedIn](state) {
+            return !!state.isLoggedIn;
+        },
+        [getterTypes.isAnonymous](state) {
+            return state.isLoggedIn === false;
+        }
+    },
     state: {
         isSubmitting: false,
+        isLoading: false,
         currentUser: null,
         validationErrors: null,
         isLoggedIn: null
@@ -47,6 +69,19 @@ const auth = {
             state.isSubmitting = false;
             state.validationErrors = payload;
         },
+        [mutationTypes.getCurrentUserStart](state){
+            state.isLoading = true;
+        },
+        [mutationTypes.getCurrentUserSuccess](state, payload){
+            state.isLoading = false;
+            state.currentUser = payload;
+            state.isLoggedIn = true;
+        },
+        [mutationTypes.getCurrentUserFailed](state){
+            state.isLoading = false;
+            state.isLoggedIn = false;
+            state.currentUser = null;
+        }
     },
     actions: {
         [actionTypes.register]({commit}, credentials){
@@ -78,6 +113,19 @@ const auth = {
                     .catch(error => {
                         commit(mutationTypes.loginFailed, error.response.data.errors)
                         console.log(error);
+                    })
+            })
+        },
+        [actionTypes.getCurrentUser]( {commit}){
+            return new Promise(resolve => {
+                commit(mutationTypes.getCurrentUserStart);
+                authApi.getCurrentUser()
+                    .then(response => {
+                        commit(mutationTypes.getCurrentUserSuccess, response.data.user);
+                        resolve(response.data.user);
+                    })
+                    .catch(() => {
+                        commit(mutationTypes.getCurrentUserFailed)
                     })
             })
         }
